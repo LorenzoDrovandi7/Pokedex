79018e81a6ce7366f4fb457f705e0b8b1acffc31
LorenzoDrovandi7 <lorenzodrovandi17@gmail.com>
2024-12-13 17:26:54 -0300

    Corrección error en la organización de carpetas

--- pokedexApi/DetallesPokemon.js
+++
@@ -1,8 +0,0 @@
-export function actualizarDetallesPokemon(pokemonData) {
-  $("#pokemon-imagen").attr("src", pokemonData.sprites.front_default);
-  $("#pokemon-nombre").text(pokemonData.name);
-  const tipos = pokemonData.types.map((tipo) => tipo.type.name).join(", ");
-  $("#pokemon-tipo").text(tipos);
-  $("#pokemon-altura").text(pokemonData.height / 10 + " m");
-  $("#pokemon-peso").text(pokemonData.weight / 10 + " kg");
-}
